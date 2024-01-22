aaf26d050fde0a58b0706c719171a3da0e0392c6
María de la Osa <m.delaosam@gmail.com>
2024-01-22 01:30:35 +0100

    bullet doesn't disappear

--- Assets/Scripts/BeeBullet.cs
+++ Assets/Scripts/BeeBullet.cs
@@ -31,7 +31,7 @@ public class EnemiesProjectile : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Suelo")
+        if (collision.gameObject.tag == "Suelo" || collision.gameObject.tag == "Enemy")
         {
             Destroy(gameObject);
         }
